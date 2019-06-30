9814ac63ecf2dc56a65e48e17806175fef5a327c
rama41222 <dinushankanrg@gmail.com>
2019-06-30 14:51:21 +0530

    feat: post component

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        
+        }
+    }
+    
+    
+}
