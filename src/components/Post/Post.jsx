0ba170a735d63723a54452a362625549f6cc2197
rama41222 <dinushankanrg@gmail.com>
2019-06-30 15:53:01 +0530

    feat: post css

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -13,11 +13,39 @@ class Post extends Component {
         this.setState({ posts: data });
     }
     
+    postTitleStyle = () => {
+        return {
+            background: '#e3e3e3',
+            padding: '10px',
+            color: '#4e82ff',
+            fontSize: '1.3em',
+            fontWeight: 'bold'
+        }
+    };
+    
+    postBodyStyle = () => {
+        return {
+            color: '#000000',
+            padding: '10px',
+            paddingBottom: '20px',
+            fontSize: '1em',
+            textAlign: 'left'
+        }
+    };
+    
+    postStyle = () => {
+        return {
+            background: '#efefef',
+            margin:'5px',
+            borderRadius: '10px'
+        }
+    };
+    
     createPosts = () => {
       return this.state.posts.map(post =>(
-          <div key={post.id}>
-              <p>{post.title}</p>
-              <p>{post.body}</p>
+          <div style={this.postStyle()} key={post.id}>
+              <p style={this.postTitleStyle()}>{post.title}</p>
+              <p style={this.postBodyStyle()}>{post.body}</p>
           </div>
       ));
     };
@@ -30,8 +58,6 @@ class Post extends Component {
             </div>
         )
     }
-    
-    
 }
 
 export default Post;
